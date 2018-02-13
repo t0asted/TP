14e28d6dfbefe7a5b609705ad965c29837f750f5
PeterRance95 <36416347+PeterRance95@users.noreply.github.com>
2018-02-13 15:38:40 +0000

    V1.4
    Player Interact

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlayInteract : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if (Input.GetKeyDown ("E"))
+        {
+            Destroy(GameObject.FindWithTag("Chest"));
+        }
 	}
 }
