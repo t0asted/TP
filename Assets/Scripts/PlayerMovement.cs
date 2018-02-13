560d4322df09bb540b8ce4193bda5c90f7546268
PeterRance95 <36416347+PeterRance95@users.noreply.github.com>
2018-02-13 13:23:52 +0000

    V1.2
    Added a player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float PlayerSpeed= 10f;
+
+    private Rigidbody2D rb2d;
+	
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizaontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * PlayerSpeed);
+    }
+}
